1e95a0f448e7f7f88c5dfcd7512c7c185c5c07a9
mac <mac@Roman-Timoshchuk.local>
2024-05-31 15:07:48 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -34,34 +34,3 @@ new Swiper('.swiper-hero', {
 		prevEl: '.swiper-button-prev-hero',
 	},
 })
-
-// Discover Slider
-new Swiper('.swiper-discover', {
-	// Optional parameters
-	loop: true,
-	spaceBetween: 30,
-
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-
-	// Responsive breakpoints
-	breakpoints: {
-		// when window width is >= 320px
-		320: {
-			slidesPerView: 1,
-		},
-
-		// when window width is >= 640px
-		640: {
-			slidesPerView: 2,
-		},
-
-		// when window width is >= 992px
-		992: {
-			slidesPerView: 3,
-		},
-	},
-})
